64f4c506da2b1560cfb8f6ac77fd1197c4896800
tsubauaaa <tsubasa1173@gmail.com>
2021-09-13 18:04:12 +0900

    ページを横向きにしないようにした

--- server_pattern/app/lib/main.dart
+++ server_pattern/app/lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:app/pages/count_page.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+    (_) {
+      runApp(
+        const ProviderScope(
+          child: MyApp(),
+        ),
+      );
+    },
   );
 }
 
